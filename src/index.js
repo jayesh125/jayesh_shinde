294249585753d2e718d3e16d76ea5c472ebdd89f
Jayesh Shinde <jayesh.v.shinde1251@gmail.com>
2024-10-23 17:26:32 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <section className='flex justify-center items-center'>
+  <section className='max-h-screen flex justify-center items-center'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
